6d0548eaf91db92441c9bdfb6292b376484b69b6
Alan Ramírez Herrera <alan5142@hotmail.com>
2021-11-24 16:57:53 -0600

    Added express validator

--- src/controllers/artists-controller.js
+++ src/controllers/artists-controller.js
@@ -1,40 +1,9 @@
 import '../data/mongo.js';
 import Artist from '../models/artist.js';
-import { validateEmail } from '../utils/validate.js';
 import { encryptPassword } from "../utils/encrypt.js";
 import Album from '../models/album.js';
 
-
-function validateArtist(name, email, password, type) {
-    if (name === undefined || email === undefined || password === undefined) {
-        throw new Error('Missing parameters');
-    }
-    if (name.length < 3) {
-        throw new Error('Name must be at least 3 characters long');
-    }
-    if (email.length < 3) {
-        throw new Error('Email must be at least 3 characters long');
-    }
-
-    if (!validateEmail(email)) {
-        throw new Error('Email is not valid');
-    }
-
-    if (password.length <= 5) {
-        throw new Error('Password must be at least 6 characters long');
-    }
-
-    if (type === undefined) {
-        throw new Error('Type must be defined');
-    }
-
-    if (type.toLowerCase() !== 'band' && type !== 'solo') {
-        throw new Error('Type must be band or solo');
-    }
-}
-
 export async function createArtist(name, email, password, description, type) {
-    validateArtist(name, email, password, type);
     const hashedPassword = await encryptPassword(password);
     const user = new Artist({
         name,
